bad5bb10a0cf5c56dece08fc5125958af81a18e2
olegnizh <olegnizh@mail.ru>
2024-02-25 22:37:12 +0300

    linq main

---
+++ ConsoleApp1 — linq/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            var selectedPeople = from p in people // промежуточная переменная p 
+                                 where p.StartsWith("А") // фильтрация по условию
+                                 orderby p // сортировка по возрастанию (дефолтная)
+                                 select p; // выбираем объект и сохраняем в выборку
+
+            foreach (string s in selectedPeople)
+                Console.WriteLine(s);
+        }
+    }
+}
