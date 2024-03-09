77118f4195f7f427fc946ef592d664be74fce1aa
olegnizh <olegnizh@mail.ru>
2024-03-09 18:32:40 +0300

    n-layer architekt with folder

---
+++ DataFinderFolder/DataFinder/BLL/BusinessRuleException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFinder.BLL
+{
+    public class BusinessRuleException : Exception
+    {
+        public BusinessRuleException(string message) : base(message) { }
+    }
+
+}
