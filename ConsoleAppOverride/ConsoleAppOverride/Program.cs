4dc4877e05b8270c4c64510632d6e946ece0850f
olegnizh <olegnizh@mail.ru>
2024-03-15 15:38:51 +0300

    override demo 1

---
+++ ConsoleAppOverride/ConsoleAppOverride/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleAppOverride
+{
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            Vehicle transport = new Truck();
+            transport.Move();
+        }
+    }
+
+    public class Vehicle
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("Moving");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Car moving");
+        }
+    }
+
+    public class Truck : Car
+    {
+        public void Move()
+        {
+            Console.WriteLine("Truck moving");
+        }
+    }
+
+}
