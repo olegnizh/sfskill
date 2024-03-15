6a21272e4f05361e76d6bf6936b1acaa6a06082b
olegnizh <olegnizh@mail.ru>
2024-03-15 16:44:22 +0300

    compile error - мы осуществляем обработку исключений в неверном порядке

---
+++ ConsoleAppException/ConsoleAppException/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Example
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                SomeMethod();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("ArgumentException caught");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block");
+            }
+        }
+
+        private static void SomeMethod()
+        {
+            throw new ArgumentException();
+        }
+    }
+}
